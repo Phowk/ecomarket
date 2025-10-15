fa5f44bcd217f05e57f69394c42907f67829995a
Phowk <estheban1023@gmail.com>
2025-10-14 23:49:16 -0500

    Añadidas las paginas: Landing Page y login; añadida la navegación entre las páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './components/landing-page/landing-page';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '' , component: LandingPage},
+    { path: 'login', component: Login},
+    { path: 'home', component: LandingPage},
+    { path: '**', pathMatch: 'full' , redirectTo: 'login' }
+];
---
+++ src/app/components/landing-page/landing-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.css'
+})
+export class LandingPage {
+
+}
